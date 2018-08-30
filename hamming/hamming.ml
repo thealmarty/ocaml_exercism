5a6ed5be2bb5d72adc9e5f5c3f3195522abdf370
martina <martina.lui@gmail.com>
2018-08-30 12:01:15 -0700

    Hamming_distance is now tail recursive.

--- hamming/hamming.ml
+++ hamming/hamming.ml
@@ -1,22 +1,18 @@
 type nucleotide = A | C | G | T
 ;;
 
-let compare_nucleotide a b =
-  match a = b with
-    true -> 0
-    |_ -> 1
-;;
-
-let rec distance s1 s2 =
+let rec sum_distance_inner n s1 s2 =
  match (s1,s2) with
-  (hd1::tl1) , (hd2::tl2) -> compare_nucleotide hd1 hd2 + distance tl1 tl2
-  |_ -> 0
+  (hd1::tl1) , (hd2::tl2) ->
+    (match hd1 = hd2 with
+      true -> sum_distance_inner n tl1 tl2
+      |_ -> sum_distance_inner (n + 1) tl1 tl2)
+  |[],[] -> Some n
+  |_ -> None
 ;;
 
 (*humming_distance takes the dna strands and returns the hamming distance.*)
-let hamming_distance (dnas1 : nucleotide list) (dnas2 : nucleotide list) =
-  match List.length dnas1 = List.length dnas2 with
-    true -> Some (distance (dnas1 : nucleotide list) (dnas2 : nucleotide list))
-    |_ -> None
-        
+let hamming_distance dnas1 dnas2 =
+  sum_distance_inner 0 dnas1 dnas2
+;;
        
