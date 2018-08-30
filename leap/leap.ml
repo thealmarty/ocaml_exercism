4e5bff6c37958a47ea53dbd67adc807cbbb60000
martina <martina.lui@gmail.com>
2018-08-30 13:17:24 -0700

    Rewrite to a single boolean expression.

--- leap/leap.ml
+++ leap/leap.ml
@@ -1,6 +1,3 @@
-let leap_year n = 
-        if n mod 4 <> 0 then false 
-        else if n mod 100 = 0 then
-                (if n mod 400 = 0 then true else false)
-        else true
-                
+let leap_year n = (n mod 4 = 0 && n mod 100 <> 0) || n mod 400 = 0
+;;
+
