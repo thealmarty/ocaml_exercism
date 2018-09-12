8afb38c0ded8b621ad2ec53785124e88b1c3be7b
Martina <martina.lui@gmail.com>
2018-09-12 11:36:10 -0700

    First try of rna-transcription.

--- rna-transcription/rna_transcription.ml
+++ rna-transcription/rna_transcription.ml
@@ -1,6 +1,17 @@
 type dna = [ `A | `C | `G | `T ]
 type rna = [ `A | `C | `G | `U ]
 
+let convert dna =
+  match dna with
+    | `A -> `U
+    | `C -> `G
+    | `G -> `C
+    | `T -> `A
+
 (** Transcribe DNA to RNA by replacing 'T' with 'U'. *)
-let to_rna dnalist =
+let rec to_rna dnalist =
+  match dnalist with
+    | hd :: tl -> convert hd :: to_rna tl
+    |_ -> []
+
         
